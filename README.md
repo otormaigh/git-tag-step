82f3793107770ac1dc73a33d56f1e5735627d1f3
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-26 20:47:18 +0100

    update run.sh

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Wercker step that can be user to tag a commit after a successful release build and deploy.
 
-Looks for an environment variable named 'STABLE_VERSION' that will be user to tag the commit.
+Looks for an environment variable named 'STABLE_VERSION' that will be used to tag the commit.
--- run.sh
+++ run.sh
@@ -1,22 +1,31 @@
 if [ ! -d ".git" ]; then
   fail "There aint no git here"
+elif [ ! -n "$WERCKER_GIT_TAG_BRANCH" ]; then
+  fail 'Missing branch property'
 fi
 
-# Configure git user
-git config --global user.email elliot@tapadoo.com
-git config --global user.name "Elliot Tormey"
+if [ "$WERCKER_RESULT" = "success" ]; then
+  # Configure git user
+  git config --global user.email elliot@tapadoo.com
+  git config --global user.name "Elliot Tormey"
 
-# Get list of tags from repo
-git fetch --tag $GIT_REMOTE
+  # Get list of tags from repo
+  info "GIT_REMOTE = $GIT_REMOTE"
+  git fetch --tag $GIT_REMOTE
 
-# Set the tag name to the stable version of the release
-tagname="$STABLE_VERSION"
+  # Set the tag name to the stable version of the release
+  tagname="$STABLE_VERSION"
 
-# Check if the tag already exists
-if [ $(git tag -l "$tagname") ]; then
-  info "A tag for this version ($tagname) already exists"
+  # Check if the tag already exists
+  if [ $(git tag -l "$tagname") ]; then
+    info "A tag for this version ($tagname) already exists"
+  else
+    info "Tagging commti $WERCKER_GIT_COMMIT"
+    # Tag and push commit
+    git tag -a tagname $WERCKER_GIT_COMMIT -m "$tagname"
+    git push --tags $GIT_REMOTE
+  fi
 else
-  # Tag and push commit
-  git tag -a tagname $WERCKER_GIT_COMMIT -m "$tagname"
-  git push --tags $GIT_REMOTE
-fi
+  info "---------------------"
+  info "Something went wrong with the previous step. Not going to tag just incase."
+  info "---------------------"
