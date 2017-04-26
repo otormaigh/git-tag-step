aa1d30e25a2c7743556521a1e2ed4995ca2ae8dc
Elliot Tormey <elliot.tormey@tapadoo.com>
2017-04-26 16:47:19 +0100

    updarte run.sh

--- run.sh
+++ run.sh
@@ -3,24 +3,20 @@ if [ ! -d ".git" ]; then
 fi
 
 # Configure git user
-if [ "$WERCKER_RESULT" = "passed" ]; then
-  git config --global user.email elliot@tapadoo.com
-  git config --global user.name "Elliot Tormey"
+git config --global user.email elliot@tapadoo.com
+git config --global user.name "Elliot Tormey"
 
-  # Get list of tags from repo
-  git fetch --tage $GIT_REMOTE
+# Get list of tags from repo
+git fetch --tage $GIT_REMOTE
 
-  # Set the tag name to the stable version of the release
-  tagname="$STABLE_VERSION"
+# Set the tag name to the stable version of the release
+tagname="$STABLE_VERSION"
 
-  # Check if the tag already exists
-  if [ $(git tag -l "$tagname") ]; then
-    info "A tag for this version ($tagname) already exists"
-  else
-    # Tag and push commit
-    git tag -a tagname $WERCKER_GIT_COMMIT -m "$tagname"
-    git push --tags $GIT_REMOTE
-  fi
+# Check if the tag already exists
+if [ $(git tag -l "$tagname") ]; then
+  info "A tag for this version ($tagname) already exists"
 else
-  info "Tagging skipped as build was not successful"
+  # Tag and push commit
+  git tag -a tagname $WERCKER_GIT_COMMIT -m "$tagname"
+  git push --tags $GIT_REMOTE
 fi
