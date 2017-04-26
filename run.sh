fce16713d0e1d5115640d4e9dfe2bcded6583dd5
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-26 21:39:53 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -5,24 +5,23 @@ elif [ ! -n "$WERCKER_GIT_TAG_BRANCH" ]; then
 fi
 
 #if [ "$WERCKER_RESULT" = "success" ]; then
+  cd $WERCKER_SOURCE_DIR
   # Configure git user
-  git config --global user.email elliot@tapadoo.com
-  git config --global user.name "Elliot Tormey"
-
-  # Get list of tags from repo
-  # info "GIT_REMOTE = $GIT_REMOTE"
-  # git fetch --tag $GIT_REMOTE
+  git config user.email elliot@tapadoo.com
+  git config user.name "Elliot Tormey"
 
   # Set the tag name to the stable version of the release
   tagname="$STABLE_VERSION"
 
   # Check if the tag already exists
-  if [ $(git tag -l "$tagname") ]; then
+  if [ git tag -l "$tagname" ]; then
     info "A tag for this version ($tagname) already exists"
   else
     info "Tagging commit $WERCKER_GIT_COMMIT"
     # Tag and push commit
     git tag -a tagname $WERCKER_GIT_COMMIT -m "$tagname"
+    # List tags just to double check.
+    git tag -l
     git push --tags $GIT_REMOTE
   fi
 #else
